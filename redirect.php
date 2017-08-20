e28b113b64821193cf1c30a12634234558047d61
Luis Faría <luisdfp@DESKTOP-DEBIAN>
2017-08-20 10:22:06 -0400

    add condition parameter to end url

--- redirect.php
+++ redirect.php
@@ -3,9 +3,9 @@ session_start();
 require ('config/config_select.php');
 
 if (parse_url($survey_url, PHP_URL_QUERY))
-  header("Location: $survey_url&pcode=".$_SESSION['participant_code']);
+  header("Location: $survey_url&pcode=".$_SESSION['participant_code']."&condition=".$_SESSION['assigned_condition']);
 else
-  header("Location: $survey_url?pcode=".$_SESSION['participant_code']);
+  header("Location: $survey_url?pcode=".$_SESSION['participant_code']."&condition=".$_SESSION['assigned_condition']);
 
 unset($_SESSION['assigned_condition']);
 unset($_SESSION['participant_code']);
