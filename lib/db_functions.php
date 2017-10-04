9fda7149519fd5a62ff96c0246e98dde5784601b
user0 <user0@web-debian>
2017-10-04 12:10:54 -0400

    conditions can now be disabled

--- lib/db_functions.php
+++ lib/db_functions.php
@@ -10,18 +10,31 @@ function get_next_condition($pdo)
 }
 
 function get_condition_with_lowest_count($pdo)
-{    
-    return $pdo->query("SELECT condition_id AS 'cond_id', count(*) as 'cond_count' FROM test_results GROUP BY condition_id ORDER BY `cond_count` ASC LIMIT 1")->fetch()['cond_id'];
+{
+    return $pdo->query(
+      "SELECT condition_id AS 'cond_id', count(*) as 'cond_count'
+       FROM test_results LEFT JOIN conditions
+       ON test_results.condition_id = conditions.id
+       WHERE conditions.enabled = 1
+       GROUP BY condition_id
+       ORDER BY `cond_count`
+       ASC LIMIT 1"
+    )->fetch()['cond_id'];
 }
 
 function get_condition_with_no_results($pdo)
 {
-    return $pdo->query("SELECT conditions.id AS 'cond_id', test_results.id AS test_id FROM conditions LEFT JOIN test_results ON conditions.id = test_results.condition_id WHERE test_results.condition_id IS null")->fetch()['cond_id'];
+    return $pdo->query(
+      "SELECT conditions.id AS 'cond_id', conditions.enabled
+       FROM conditions LEFT JOIN test_results
+       ON conditions.id = test_results.condition_id
+       WHERE test_results.condition_id IS null AND conditions.enabled = 1"
+    )->fetch()['cond_id'];
 }
 
 function insert_into($pdo, $table, array $key_value)
 {
-    $sql = "INSERT INTO $table "; 
+    $sql = "INSERT INTO $table ";
     $columns = "(";
     $values = "(";
     foreach ($key_value as $key => $val) {
@@ -31,6 +44,6 @@ function insert_into($pdo, $table, array $key_value)
     $columns = rtrim($columns, ", ") . ")";
     $values = rtrim($values, ", ") . ")";
     $sql .= "$columns VALUES $values;";
-    $stmt = $pdo->prepare($sql);    
+    $stmt = $pdo->prepare($sql);
     return $stmt->execute($key_value);
 }
